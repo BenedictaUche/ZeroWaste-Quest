f81a8f9cfd764d8ac42acd2fe59eae825d2fe1e5
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2023-12-29 07:29:43 +0100

    feat: added theme changer

--- components/user-nav.tsx
+++ components/user-nav.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from 'react'
-
+import { useUser } from './UserContext'
 import {
     Avatar,
     AvatarFallback,
@@ -34,10 +34,12 @@ import {
 
 
 
+
   export function UserNav() {
 
     const [isLoggedIn, setIsLoggedIn] = useState(true)
     const [showEditprofile, setShowEditProfile] = useState(false)
+    const user = useUser()
 
     const SHEET_SIDES = ["top", "right", "bottom", "left"] as const
 
@@ -59,9 +61,9 @@ import {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-            <Avatar className="h-8 w-8">
-              <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-              <AvatarFallback>SC</AvatarFallback>
+            <Avatar className="h-8 w-8 bg-gray-200">
+              <AvatarImage src="/avatars/01.png" alt={user?.username} />
+              <AvatarFallback>{user?.username.charAt(0).toUpperCase()}{user?.username.charAt(1).toUpperCase()}</AvatarFallback>
             </Avatar>
           </Button>
         </DropdownMenuTrigger>
@@ -69,9 +71,9 @@ import {
             <DropdownMenuContent className="w-56" align="end" forceMount>
             <DropdownMenuLabel className="font-normal">
               <div className="flex flex-col space-y-1">
-                <p className="text-sm font-medium leading-none">shadcn</p>
+                <p className="text-sm font-medium leading-none">{user?.username}</p>
                 <p className="text-xs leading-none text-muted-foreground">
-                  m@example.com
+                  {user?.email}
                 </p>
               </div>
             </DropdownMenuLabel>
